ce1b3c2b5522f7b1e3a41aad44634ce3d972294d
anass Touati <anass.touati@hagergroup-external.com>
2023-06-23 21:08:34 +0200

    code clean up

--- App/RunStoreManager.php
+++ App/RunStoreManager.php
@@ -9,10 +9,14 @@ declare(strict_types=1);
 
 namespace AnassTouatiCoder\MultiStoreRun\App;
 
+use Magento\Framework\App\DeploymentConfig;
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\ObjectManagerFactory;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Exception\RuntimeException;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\Store;
 use Magento\Store\Model\StoreManager;
 
 /**
@@ -20,13 +24,14 @@ use Magento\Store\Model\StoreManager;
  */
 class RunStoreManager
 {
-    const CONFIG_DATA_TABLE = 'core_config_data';
-    const CONFIG_BASE_URL_PATH = 'web/secure/base_url';
-    const CONFIG_SCOPE_ID = 'scope_id';
-    const CONFIG_SCOPE= 'scope';
-    const VALUE_SCOPE= 'value';
+    protected const CONFIG_DATA_TABLE = 'core_config_data';
+    protected const CONFIG_SCOPE_ID = 'scope_id';
+    protected const CONFIG_SCOPE= 'scope';
+    protected const VALUE_SCOPE= 'value';
 
-    /** @var array  */
+    /**
+     * @var array
+     */
     protected $initParams = [];
 
     /**
@@ -39,12 +44,33 @@ class RunStoreManager
      */
     protected static $instance = null;
 
-    /** @var string  */
-    protected $scopeType;
     /**
-     * @var mixed|string
+     * @var string
+     */
+    protected $scopeType;
+
+    /**
+     * @var string
      */
     protected $baseURL;
+
+    /**
+     * Main entry
+     *
+     * @param ObjectManagerFactory|null $factory
+     * @param array $initParams
+     *
+     * @return array
+     */
+    public static function getUpdatedInitParams($factory, $initParams)
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($factory, $initParams);
+            self::$instance->applyRunCodeAndType();
+        }
+
+        return self::$instance->initParams;
+    }
 
     /**
      * RunCodeManager constructor.
@@ -60,7 +86,29 @@ class RunStoreManager
     {
         $this->initParams = $initParams;
         $this->objectManager = $factory->create($this->initParams);
-        $deploymentConfig = $this->objectManager->get(\Magento\Framework\App\DeploymentConfig::class);
+    }
+
+    /**
+     * Detect and apply MAGE_RUN_CODE and MAGE_RUN_TYPE for current store view, website or admin
+     *
+     * @return void
+     */
+    private function applyRunCodeAndType()
+    {
+        $deploymentConfig = $this->objectManager->get(DeploymentConfig::class);
+
+        try {
+            // Get admin frontName from env.php or config.php file
+            $backendFrontName = $deploymentConfig->get('backend/frontName');
+            // Using strpos instead of contains to stay compatible with old PHP versions
+            $subdomain = parse_url($this->initParams['REQUEST_URI'], PHP_URL_PATH);
+            if (strpos($subdomain, '/' . $backendFrontName)!== false ||
+                strpos($this->initParams['SERVER_NAME'], 'admin')!== false) {
+                return;
+            }
+        } catch (FileSystemException|RuntimeException $e) {
+            return;
+        }
 
         $scopeId = $this->retrieveId($this->getFullURL($this->initParams));
         $runCode = '';
@@ -74,14 +122,6 @@ class RunStoreManager
                 $this->initParams['PHP_SELF'] = "/{$subDirectory}/index.php";
             }
         } else {
-            // Get admin frontName from env.php or config.php file
-            $backendFrontName = $deploymentConfig->get('backend/frontName');
-            // Using strpos instead of contains to stay compatible with old PHP versions
-            $subdomain = parse_url($this->initParams['REQUEST_URI'], PHP_URL_PATH);
-            if (strpos($subdomain, '/' . $backendFrontName)!== false ||
-                strpos($this->initParams['SERVER_NAME'], 'admin')!== false) {
-                $runCode = 'admin';
-            }
             // if you don't want to manage default home page
             // else {
 
@@ -89,30 +129,14 @@ class RunStoreManager
             // exit;
             //}
         }
-        $this->initParams[\Magento\Store\Model\StoreManager::PARAM_RUN_CODE] = $runCode ?: 'base';
-        $this->initParams[\Magento\Store\Model\StoreManager::PARAM_RUN_TYPE] = $this->scopeType ?: 'website';
+        $this->initParams[StoreManager::PARAM_RUN_CODE] = $runCode ?: 'base';
+        $this->initParams[StoreManager::PARAM_RUN_TYPE] = $this->scopeType ?: 'website';
 
         $_SERVER = $this->initParams;
     }
-    /**
-     * Main entry
-     *
-     * @param ObjectManagerFactory|null $factory
-     * @param array $initParams
-     *
-     * @return array
-     */
-    public static function getUpdatedInitParams($factory, $initParams)
-    {
-        if (self::$instance === null) {
-            self::$instance = new self($factory, $initParams);
-        }
-
-        return self::$instance->initParams;
-    }
 
     /**
-     * Build full url from request
+     * Build full url without params from request
      *
      * @param array $initParams
      * @return string
@@ -122,11 +146,11 @@ class RunStoreManager
         $requestedDomain = $initParams['HTTP_HOST'];
         $subdomain = parse_url($initParams['REQUEST_URI'], PHP_URL_PATH);
 
-        return ($initParams['SERVER_PORT'] == 443 ? 'https' : 'http') . '://' . $requestedDomain . $subdomain;
+        return 'https://' . $requestedDomain . $subdomain;
     }
 
     /**
-     * Get website code by id
+     * Get store view or website code by id
      *
      * @param int $id
      * @return string
@@ -136,8 +160,6 @@ class RunStoreManager
         $code = '';
         $storeManager = $this->objectManager->get(StoreManager::class);
         if ($this->scopeType === ScopeInterface::SCOPE_WEBSITE) {
-            /** @var StoreManager $storeManager */
-            /** @var \Magento\Store\Api\Data\WebsiteInterface[] $websites */
             $websites = $storeManager->getWebsites(true, false);
             $code = $websites[$id]->getCode();
         } elseif ($this->scopeType === ScopeInterface::SCOPE_STORE) {
@@ -166,7 +188,7 @@ class RunStoreManager
                 ['config' => $resource->getTableName(self::CONFIG_DATA_TABLE)],
                 [self::CONFIG_SCOPE, self::CONFIG_SCOPE_ID, self::VALUE_SCOPE]
             )
-            ->where('config.path = ?', self::CONFIG_BASE_URL_PATH)
+            ->where('config.path = ?', Store::XML_PATH_SECURE_BASE_URL)
             ->where(new \Zend_Db_Expr('LOCATE(value, ?) = 1'), $domain)
             ->order('LENGTH(config.value) DESC')
             ->limit(1);
